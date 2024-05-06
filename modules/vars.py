04cf3ccc453b5e1f74b593f8cebe7cddc7ec88c5
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-06 06:45:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "28528007"
 api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "6728214327:AAFm745olGnL8Jfr4yCR3kKUdk-A9qo9ObU"
+bot_token = "6634064190:AAH8iv5BXtIp_c4ucv3kgcR3MpN25GdSaXA"
